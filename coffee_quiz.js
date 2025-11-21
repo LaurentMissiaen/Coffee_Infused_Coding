76dab44ac13776f4658877fdb1658ef42e4bd1ed
Larry Missiaen <lorremissiaen@gmail.com>
2025-11-21 13:37:46 +0100

    add simple return value logic

--- coffee_quiz.js
+++ coffee_quiz.js
@@ -1,1 +1,24 @@
-console.log("HI FROM QUIZ FILE!");
+const firstQuestion = document.getElementById("firstQuestion");
+const secondQuestion = document.getElementById("secondQuestion");
+const thirdQuestion = document.getElementById("thirdQuestion");
+const fourthQuestion = document.getElementById("fourthQuestion");
+const fifthQuestion = document.getElementById("fifthQuestion");
+
+const button = document.getElementById("btn");
+const answer = document.getElementById("answer");
+
+const calculateAnswer = function () {
+  if (
+    firstQuestion.value === "firstOption" &&
+    secondQuestion.value === "firstOption" &&
+    thirdQuestion.value === "firstOption" &&
+    fourthQuestion.value === "firstOption" &&
+    fifthQuestion.value === "firstOption"
+  ) {
+    return "POUR OVER";
+  } else return "SOMETHING ELSE";
+};
+
+button.addEventListener("click", () => {
+  answer.innerHTML = calculateAnswer();
+});
